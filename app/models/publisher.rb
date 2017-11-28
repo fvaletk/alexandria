f8b45e1a6410047ffa6563788c836d5d195224e4
Filiberto Valetk <fili.valetk@gmail.com>
2017-11-28 11:30:24 -0500

    [ALEXA-003] Add Annotate gem

--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -1,5 +1,15 @@
-class Publisher < ApplicationRecord
+# == Schema Information
+#
+# Table name: publishers
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 
+# app/models/publisher.rb
+class Publisher < ApplicationRecord
   # Validations
   validates :name, presence: true
 end
