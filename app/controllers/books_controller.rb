a696c75d689f8bbb252ca4de1cbf73069a5a6a0d
Filiberto Valetk <fili.valetk@gmail.com>
2017-12-01 17:13:02 -0500

    [ALEXA-007] Add BooksController

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,6 @@
+# app/controllers/books_controller.rb
+class BooksController < ApplicationController
+  def index
+    render json: { data: Book.all }
+  end
+end
--- app/uploaders/cover_uploader.rb
+++ app/uploaders/cover_uploader.rb
@@ -17,7 +17,7 @@ class CoverUploader < CarrierWave::Uploader::Base
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url
-    url = "uploads/#{models.class.to_s.underscore}/#{mounted_as}/default/cover.jpg"
+    url = "uploads/#{model.class.to_s.underscore}/#{mounted_as}/default/cover.jpg"
     url.prepend('/') unless url[0] == '/'
     url
   end
