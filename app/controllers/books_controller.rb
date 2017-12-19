6f30b4a3435b5f9e0d9e02bc0adb9428a1359678
Filiberto Valetk <fili.valetk@gmail.com>
2017-12-19 16:58:49 -0500

    [ALEXA-022] Adding AlexandriaSerializer

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,9 @@
 # app/controllers/books_controller.rb
 class BooksController < ApplicationController
   def index
-    books = orchestrate_query(Book.all).map do |book|
-      BookPresenter.new(book, params).fields.embeds
-    end
+    books = orchestrate_query(Book.all)
+    serializer = Alexandria::Serializer.new(data: books, params: params, actions: [:fields, :embeds])
 
-    render json: { data: books }.to_json
+    render json: serializer.to_json
   end
 end
