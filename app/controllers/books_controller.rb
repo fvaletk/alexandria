a72bf3e4022529eff5d8c4703288fb2b30d4ec2a
Filiberto Valetk <fili.valetk@gmail.com>
2018-03-20 14:04:05 -0500

    [ALEXA-024] Add Books create method

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,10 +9,21 @@ class BooksController < ApplicationController
     render serializer(book)
   end
 
+  def create
+    book = Book.create(book_params)
+    render serializer(book).merge(status: :created, location: book)
+  end
+
   private
 
   def book
     @book ||= params[:id] ? Book.find_by!(id: params[:id]) : Book.new(book_params)
   end
   alias_method :resource, :book
+
+  def book_params
+    params.require(:data).permit(:title, :subtitle, :isbn_10, :isbn_13,
+                                 :description, :released_on, :publisher_id,
+                                 :author_id, :cover)
+  end
 end
