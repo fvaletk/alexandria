c24fbe4789ee94c6d0c500be2ff685e822ac8c01
Filiberto Valetk <fili.valetk@gmail.com>
2018-03-22 17:21:10 -0500

    [ALEXA-028] Add AuthorsController

---
+++ spec/requests/authors_spec.rb
@@ -0,0 +1,190 @@
+# spec/requests/authors_spec.rb
+
+require 'rails_helper'
+
+RSpec.describe 'Authors', type: :request do
+  let(:pat) { create(:author) }
+  let(:michael) { create(:michael_hartl) }
+  let(:sam) { create(:sam_ruby) }
+  let(:authors) { [pat, michael, sam] }
+
+  describe 'GET /api/authors' do
+    before { authors }
+
+    context 'default behavior' do
+      before { get('/api/authors') }
+
+      it 'receives HTTP status 200' do
+        expect(response.status).to eq 200
+      end
+
+      it 'receives a json with the "data" root key' do
+        expect(json_body['data']).to_not be nil
+      end
+
+      it 'receives all 3 books' do
+        expect(json_body['data'].size).to eq 3
+      end
+    end
+
+    describe 'field picking' do
+      context 'with the fields parameter' do
+        before { get('/api/authors?fields=id,given_name,family_name') }
+
+        it 'gets authors with only the id, given_name and family_name' do
+          json_body['data'].each do |author|
+            expect(author.keys).to eq(['id', 'given_name', 'family_name'])
+          end
+        end
+      end
+
+      context 'without the "field" parameter' do
+        before { get '/api/authors' }
+
+        it 'gets authors with all the fields specified in the presenter' do
+          json_body['data'].each do |author|
+            expect(author.keys).to eq AuthorPresenter.build_attributes.map(&:to_s)
+          end
+        end
+      end
+
+      context 'with invalid field name "fid"' do
+        before { get '/api/authors?fields=fid,given_name,family_name' }
+
+        it 'gets "400 Bad Request" back' do
+          expect(response.status).to eq 400
+        end
+
+        it 'receives an error' do
+          expect(json_body['error']).to_not be nil
+        end
+
+        it 'receives "fields=fid" as an invalid params' do
+          expect(json_body['error']['invalid_params']).to eq 'fields=fid'
+        end
+      end
+    end
+
+    describe 'pagination' do
+      context 'when asking for the first page' do
+        before { get '/api/authors?page=1&per=2' }
+
+        it 'receives HTTP status 200' do
+          expect(response.status).to eq 200
+        end
+
+        it 'receives only two books' do
+          expect(json_body['data'].size).to eq 2
+        end
+
+        it 'receives a response with the Link Header' do
+          expect(response.headers['Link'].split(', ').first).to eq(
+            '<http://www.example.com/api/authors?page=2&per=2>; rel="next"'
+          )
+        end
+      end
+
+      context 'when asking for the second page' do
+        before { get('/api/authors?page=2&per=2') }
+
+        it 'receives HTTP status 200' do
+          expect(response.status).to eq 200
+        end
+
+        it 'receives only one author' do
+          expect(json_body['data'].size).to eq 1
+        end
+      end
+
+      context 'when sending invalid "page" and "per" parameters' do
+        before { get('/api/authors?page=fake&per=10') }
+
+        it 'receives HTTP status 400' do
+          expect(response.status).to eq 400
+        end
+
+        it 'receives an error' do
+          expect(json_body['error']).to_not be nil
+        end
+
+        it 'receives "page=fake" as an invalid param' do
+          expect(json_body['error']['invalid_params']).to eq 'page=fake'
+        end
+      end
+    end
+
+    describe 'sorting' do
+      context 'with valid column name "id"' do
+        it 'sorts the authors by "id desc"' do
+          get('/api/authors?sort=id&dir=desc')
+          expect(json_body['data'].first['id']).to eq sam.id
+          expect(json_body['data'].last['id']).to eq pat.id
+        end
+      end
+
+      context 'with invalid column name "fid"' do
+        before { get('/api/authors?sort=fid&dir=asc') }
+
+        it 'get "400 Bad Request" back' do
+          expect(response.status).to eq 400
+        end
+
+        it 'receives an error' do
+          expect(json_body['error']).to_not be nil
+        end
+
+        it 'receives "sort=fid" as an invalid param' do
+          expect(json_body['error']['invalid_params']).to eq 'sort=fid'
+        end
+      end
+    end
+
+    describe 'filtering' do
+      context 'with valid filtering param "q[given_name_cont]=Pat"' do
+        it 'receives "Pat" back' do
+          get('/api/authors?q[given_name_cont]=Pat')
+          expect(json_body['data'].first['id']).to eq pat.id
+          expect(json_body['data'].size).to eq 1
+        end
+      end
+
+      context 'with invalid filtering param "q[fgiven_name_cont]=Pat"' do
+        before { get('/api/authors?q[fgiven_name_cont]=Pat') }
+
+        it 'gets "400 Bad Request" back' do
+          expect(response.status).to eq 400
+        end
+
+        it 'receives an error' do
+          expect(json_body['error']).to_not be nil
+        end
+
+        it 'receives "q[fgiven_name_cont]=Pat" as an invalid param' do
+          expect(json_body['error']['invalid_params']).to eq 'q[fgiven_name_cont]=Pat'
+        end
+      end
+    end
+  end
+
+  describe 'GET /api/authors/:id' do
+    context 'with existing resource' do
+      before { get("/api/authors/#{pat.id}") }
+
+      it 'gets HTTP status 200' do
+        expect(response.status).to eq 200
+      end
+
+      it 'receives the "pat" author as JSON' do
+        expected = { data: AuthorPresenter.new(pat, {}).fields.embeds }
+        expect(response.body).to eq(expected.to_json)
+      end
+    end
+
+    context 'with nonexisting resource' do
+      it 'get HTTP status 404' do
+        get '/api/authors/2314242'
+        expect(response.status).to eq 404
+      end
+    end
+  end
+end
